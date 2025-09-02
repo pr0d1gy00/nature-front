9df0ed1b5c8dfb0c64c119e6a1286cb1d24f35e8
prodigy00 <calitoalejandro184@gmail.com>
2025-09-02 15:48:20 -0400

    modified order page and create utils

--- app/orders/my-orders/page.tsx
+++ app/orders/my-orders/page.tsx
@@ -10,32 +10,7 @@ import {jwtDecode} from "jwt-decode";
 import {decryptId} from "@/helpers/decryptedId";
 import Button from "@/components/button";
 import ShowDetailsOrderModal from "@/components/showDetailsOrderModal";
-
-export function formatDate(dateString: string) {
-    const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit'
-    };
-    return new Date(dateString).toLocaleDateString('es-ES', options);
-};
-
-export function getStatusColor(status: string){
-    switch (status.toLowerCase()) {
-        case 'pendiente':
-            return 'bg-yellow-200 text-yellow-800';
-        case 'pagado':
-            return 'bg-blue-200 text-blue-800';
-        case 'enviado':
-            return 'bg-indigo-200 text-indigo-800';
-        case 'cancelado':
-            return 'bg-red-200 text-red-800';
-        default:
-            return 'bg-gray-200 text-gray-800';
-    }
-};
+import {formatDate, getStatusColor} from "@/utils/order";
 export default function Page() {
     const {
         allOrders,
